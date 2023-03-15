bbd467b925afd2d7608be6fd2a6a9481699c3df5
CHENG-YOU LU <A2694815@gmail.com>
2023-03-15 17:56:15 -0400

    Pass new camera pose into undistort function.
    Pass new camera pose into undistort function and update intrinsic with new camera pose in json file.

--- scripts/undistort_per_cam.py
+++ scripts/undistort_per_cam.py
@@ -21,21 +21,21 @@ for i in range(len(jd['frames'])):
     p1 = jd['frames'][i]['p1']
     p2 = jd['frames'][i]['p2']
     dist = np.array([k1, k2, p1, p2])
+	# update pose
+    new_pose = cv.getOptimalNewCameraMatrix(pose, dist, [1280, 720], alpha=1)
     for j in range(0, 2, 1):
         path = jd['frames'][i]['file_path']
         cam, time = path.split('/')[-1].split('_')
         time = int(time.split('.')[0])+j
         print(root, cam, time)
         src = cv.imread(os.path.join(root, "image", "segmented", f"{cam}_{time:04d}.png"), cv.IMREAD_UNCHANGED)
-        dst = cv.undistort(src, pose, dist, None, None)
+        dst = cv.undistort(src, pose, dist, None, new_pose)
         cv.imwrite(os.path.join(".", root, "image", "undist", f"{cam}_{time:04d}.png"), dst)
         print(os.path.join(".", root, "image", "undist", f"{cam}_{time:04d}.png"))
-    # update pose
-    new_pose = cv.getOptimalNewCameraMatrix(pose, dist, [1280, 720], alpha=1)
-    jd['frames'][i]['fl_x'] = pose[0, 0]
-    jd['frames'][i]['fl_y'] = pose[1, 1]
-    jd['frames'][i]['cx'] = pose[0, 2]
-    jd['frames'][i]['cy'] = pose[1, 2]
+    jd['frames'][i]['fl_x'] = new_pose[0, 0]
+    jd['frames'][i]['fl_y'] = new_pose[1, 1]
+    jd['frames'][i]['cx'] = new_pose[0, 2]
+    jd['frames'][i]['cy'] = new_pose[1, 2]
     # remove k1k2p1p2
     del jd['frames'][i]['k1']
     del jd['frames'][i]['k2']
